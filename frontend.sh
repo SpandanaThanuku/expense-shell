27139073101f7710ad0d2a12d19145d741211c6f
Spandana <spandanathanuku3@gmail.com>
2025-07-21 22:42:29 +0530

    created common file and using as source file in frontend and backend

--- frontend.sh
+++ frontend.sh
@@ -7,34 +7,34 @@ head() {
 
 head "Installing nginx"
 dnf install nginx -y &>>$log_file
-echo  $?
+echo $?
 
 head "copying config files"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
-echo  $?
+echo $?
 
 
 head "removing old/default content"
 rm -rf /usr/share/nginx/html/* &>>$log_file
-echo  $?
+echo $?
 
 head "downloading frontend code"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
-echo  $?
+echo $?
 
 cd /usr/share/nginx/html &>>$log_file
-echo  $?
+echo $?
 
 
 head "extracting the code"
 unzip /tmp/frontend.zip &>>$log_file
 
-echo  $?
+echo $?
 
 head "enabling nginx"
 systemctl enable nginx &>>$log_file
 
-echo  $?
+echo $?
 
 head "restarting nginx"
 systemctl restart nginx &>>$log_file
