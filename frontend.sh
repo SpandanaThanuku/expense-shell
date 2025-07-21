42043ba9c4c67700ba4edd8a659f2ccfd8dd6d54
Spandana <spandanathanuku3@gmail.com>
2025-07-21 22:05:30 +0530

    created headings and log file

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,27 @@
-# Installing nginx
-dnf install nginx -y
-cp expense.conf /etc/nginx/default.d/expense.conf
+log_file=/tmp/expense.log
 
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+#declaring function
+head() {
+  echo -e "\e[35m$1\e[0m"
+}
 
-systemctl enable nginx
-systemctl restart nginx
+head "Installing nginx"
+dnf install nginx -y &>>$log_file
+
+head "copying config files"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+
+head "removing old/default content"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+head "downloading frontend code"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+cd /usr/share/nginx/html &>>$log_file
+
+head "extracting the code"
+unzip /tmp/frontend.zip &>>$log_file
+
+head "enabling nginx"
+systemctl enable nginx &>>$log_file
+
+head "restarting nginx"
+systemctl restart nginx &>>$log_file
